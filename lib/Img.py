c78ef5e46d9d3d24caad99965438c746f64f22e8
massimilianocomin <mass.comin92@gmail.com>
2017-04-07 12:51:32 -0400

    Update Img.py

--- lib/Img.py
+++ lib/Img.py
@@ -8,10 +8,25 @@ Created on Tue Mar 21 11:44:58 2017
 import os
 import sys
 import numpy as np
+
+if os.environ['LOC'] == 'local':
+    path = '/Network/Servers/seguin.pmc.umontreal.ca/Users/mcomin/inpainting'
+    libpath = '/Users/mcomin/Google Drive/Master/Codes/lib'
+
+elif os.environ['LOC'] == 'hades':
+    path = '/home2/ift6ed13/data'
+    libpath = '/home2/ift6ed13/lib'
+    import matplotlib
+    matplotlib.use('Agg')
+else:
+    sys.exit('Environment variable LOC not found. Verify .bash_profile.')
+    
+    
 import matplotlib.pyplot as plt
 import PIL.Image as Image
 import theano
 import glob
+import pickle
 
 theano.config.floatX = 'float32'
 theano.config.intX = 'int32'
@@ -20,102 +35,109 @@ theano.config.intX = 'int32'
 class Img:
     
     def __init__(self):
+
+        with open(libpath+'/SKIP_NAMES','rb') as file:
+            self.IMG_TO_SKIP = pickle.load(file)
+
+        trainlist = glob.glob(path+'/train/*.jpg')
+        validlist = glob.glob(path+'/valid/*.jpg')
         
-        if os.environ['LOC'] == 'local':
-            self.path = '/Network/Servers/seguin.pmc.umontreal.ca/Users/mcomin/inpainting'
-        elif os.environ['LOC'] == 'hades':
-            self.path = '/home2/ift6ed13/data'
-        else: 
-            sys.exit('Environment variable LOC not found. Verify .bash_profile.')
+        self.trainlist = [x for x in trainlist if x not in self.IMG_TO_SKIP]
+        self.validlist = [x for x in validlist if x not in self.IMG_TO_SKIP]
+
+
     def load(self,n=None):
-        
-        m = None if n is None else 2*n
+        """
+        Loads the whole dataset, or the first n examples.
+        """
         
         print('Loading COCO dataset...')
-        trainlist = glob.glob(self.path+'/train'+'/*.jpg')
-        validlist = glob.glob(self.path+'/valid'+'/*.jpg')
-        traincroplist = glob.glob(self.path+'/train_crop'+'/*.jpg')
-        validcroplist = glob.glob(self.path+'/valid_crop'+'/*.jpg')
         
-        train = [np.array(Image.open(fname)) for fname in trainlist[0:m]]
+        train = [np.array(Image.open(fname)) for fname in self.trainlist[0:n]]
         train = np.array([x for x in train if x.shape == (64,64,3)])
         train = (train[0:n]/256).astype(theano.config.floatX)
+        train = train.transpose(0,3,1,2)
 
-        valid = [np.array(Image.open(fname)) for fname in validlist[0:m]]
+        valid = [np.array(Image.open(fname)) for fname in self.validlist[0:n]]
         valid = np.array([x for x in valid if x.shape == (64,64,3)])
         valid = (valid[0:n]/256).astype(theano.config.floatX)
-
-        train_crop = [np.array(Image.open(fname)) for fname in traincroplist[0:m]]
-        train_crop = np.array([x for x in train_crop if x.shape == (64,64,3)])
-        train_crop = (train_crop[0:n]/256).astype(theano.config.floatX)
-
-        valid_crop = [np.array(Image.open(fname)) for fname in validcroplist[0:m]]
-        valid_crop = np.array([x for x in valid_crop if x.shape == (64,64,3)])
-        valid_crop = (valid_crop[0:n]/256).astype(theano.config.floatX)
-        
-        train = train.transpose(0,3,1,2)
         valid = valid.transpose(0,3,1,2)
-        train_crop = train_crop.transpose(0,3,1,2)
-        valid_crop = valid_crop.transpose(0,3,1,2)
-        
-        trainset = train_crop, train
-        validset = valid_crop, valid
+
+        train_crop = np.copy(train)
+        train_crop[:,:,16:48,16:48] = 0
+        valid_crop = np.copy(valid)
+        valid_crop[:,:,16:48,16:48] = 0
+
         print('Dataset loaded.')
 
-        return trainset, validset
-    
-    def plot(self,inp,tar=None):
+        return train_crop, train, valid_crop, valid
+
+
+    def load_batch(self,batchsize,i,mode):
+        """
+        Loads successive minibatches of the dataset.
+        """
         
-        if tar is not None:
-            image = np.concatenate((inp.transpose(1,2,0),tar.transpose(1,2,0)),axis=1)
+        if mode == 'train':
+            batch_list = self.trainlist[i*batchsize:(i+1)*batchsize]
+        elif mode == 'valid':
+            batch_list = self.validlist[i*batchsize:(i+1)*batchsize]
         else:
-            image = inp.transpose(1,2,0)
+            sys.exit('Img.load_batch Error: Please select a valid mode.')
+            
+        batch = [np.array(Image.open(fname)) for fname in batch_list]
+        batch = np.array(batch[0:batchsize],dtype=theano.config.floatX)/256.
+        batch = batch.transpose(0,3,1,2)
+
+        batch_crop = np.copy(batch)
+        batch_crop[:,:,16:48,16:48] = 0
+        
+        return batch_crop, batch[:,:,16:48,16:48]
+
+
+    def plot(self,inp,imgpath=None):
+        
+        image = inp.transpose(1,2,0)
 
         plt.axis("off")
         plt.imshow(image)
-
-    def crop(self,img):
-
-        tocrop = np.array(img)
-        center = (int(np.floor(tocrop.shape[0] / 2.)), int(np.floor(tocrop.shape[1] / 2.)))
-        
-        if len(tocrop.shape) == 3:
-            tocrop[center[0]-16:center[0]+16, center[1]-16:center[1]+16, :] = 0
-        else:
-            tocrop[center[0]-16:center[0]+16, center[1]-16:center[1]+16] = 0
-        
-        cropped_image = Image.fromarray(tocrop)
-        
-        return cropped_image
-
+        if imgpath:
+            plt.savefig(imgpath+'.png')
     
-    def _GENERATE_CROPPED_IMG(self):
+    def save(self,image,imgpath):
         
-        print('Loading images from path...')
-        train = glob.glob(self.path+'/train/*.jpg')
-        valid = glob.glob(self.path+'/valid/*.jpg')
-        train_crop = glob.glob(self.path+'/train_crop/*.jpg')
-        valid_crop = glob.glob(self.path+'/valid_crop/*.jpg')
+        image = image.transpose(1,2,0)
         
-        print('Verifying already cropped images...')
-        train_todo = [x for x in train if x not in set(train_crop)]
-        valid_todo = [x for x in valid if x not in set(valid_crop)]
-        print('Cropping images...')
+        with open(imgpath,'wb') as file:
+            pickle.dump(image,file, 2)
 
-        for image_path in valid_todo:
-            try:
-                img = Image.open(image_path)
-                img = self.crop(img)
-                name = os.path.basename(image_path)
-                img.save( self.path + '/valid_crop/' + name)
-            except:
-                print('An error occured while cropping ',name,'. Image skipped.')
+    def _DETECT_GRAYSCALE_IMG(self):
+        
+        skipnames = []
+        
+        for name in self.trainlist:
+            op = Image.open(name)
+            img = np.array(op)
+            if img.shape != (64,64,3):
+                skipnames += [name]
+                print('Found Error')
+            op.close()
 
-        for image_path in train_todo:
-            try:
-                img = Image.open(image_path)
-                img = self.crop(img)
-                name = os.path.basename(image_path)
-                img.save( self.path + '/train_crop/' + name)
-            except:
-                print('An error occured while cropping ',name,'. Image skipped.')
+        for name in self.validlist:
+            op = Image.open(name)
+            img = np.array(op)
+            if img.shape != (64,64,3):
+                skipnames += [name]
+                print('Found Error')
+            op.close()
+        
+        with open('SKIP_NAMES','wb') as file:
+            pickle.dump(skipnames,file,-1)
+
+I = Img()
+#for i in range(80000//100):
+#    test = I.load_batch(100,i,mode='train')
+#    if test[0].shape != (100, 3, 64, 64):
+#        print('ERROR 0')
+#    if test[1].shape != (100, 3, 32, 32):
+#        print('ERROR 1')
