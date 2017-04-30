f6052ab939582658ad678a3f8918a0dcafb3a5e0
massimilianocomin <mass.comin92@gmail.com>
2017-04-30 14:54:16 -0400

    Submit for Hades

--- submit.sh
+++ submit.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-#PBS -l walltime=48:00:00
-#PBS -l nodes=1:ppn=2 -l mem=12gb
+#PBS -l walltime=120:00:00
+#PBS -l nodes=ngpu-a4-09:ppn=2 -l mem=12gb
+#PBS -o /home2/ift6ed13/results/log.out
 #PBS -r n
 
 module add python/3.5.1
 module add openblas/0.2.18
 module add CUDA/7.5
 
+cd /home2/ift6ed13/
+
+cp -rf /home2/ift6ed13/data $LSCRATCH
+cp /home2/ift6ed13/code/Launch.py $LSCRATCH
+cp /home2/ift6ed13/code/LSGAN.py $LSCRATCH
+
+cd $LSCRATCH
+
 source /home2/ift6ed13/p3.5/bin/activate
 
-python /home2/ift6ed13/code/Launch.py
+python Launch.py
+
+rm -rf *
+
